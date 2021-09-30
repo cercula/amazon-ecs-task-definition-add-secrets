f5f3c00e7e2c88d2c8fe0f2e680b6ed049db55c2
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2021-09-30 17:00:03 +0100

    adds project setup

---
+++ index.js
@@ -0,0 +1,28 @@
+const core = require("@actions/core")
+const github = require("@actions/github")
+
+try {
+  const taskDefinition = core.getInput("task-definition")
+  const secrets = core.getInput("secrets")
+
+  const taskDefinitionPath = path.isAbsolute(taskDefinition)
+    ? taskDefinition
+    : path.join(process.env.GITHUB_WORKSPACE, taskDefinition)
+  if (!fs.existsSync(taskDefinitionPath)) {
+    throw new Error(`Task definition file does not exist: ${taskDefinition}`)
+  }
+
+  const secretsPath = path.isAbsolute(secrets)
+    ? secrets
+    : path.join(process.env.GITHUB_WORKSPACE, secrets)
+  if (!fs.existsSync(secretsPath)) {
+    throw new Error(`Secrets file does not exist: ${secrets}`)
+  }
+
+  const taskDefinitionContents = require(taskDefinitionPath)
+  const secretsContents = require(secretsPath)
+
+  console.log(taskDefinitionContents, secretsContents)
+} catch (error) {
+  core.setFailed(error.message)
+}
